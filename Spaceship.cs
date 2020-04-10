85318e4d8c3611382b7e0d368e516b4a427d1a47
Skiddy Gamer <karels.jonass@gmail.com>
2020-04-10 16:29:32 +0300

    s

---
+++ Spaceship.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms; 
+
+namespace Space_battle
+{
+    class Spaceship : PictureBox
+    {
+        public Spaceship()
+        {
+            InitializeSpaceship();  
+        }
+
+        private void InitializeSpaceship()
+        {
+            this.BackColor = Color.Salmon;
+            this.Height = 40;
+            this.Width = 100;
+        }
+    }
+}
